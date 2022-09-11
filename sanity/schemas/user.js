0d425e3b95f3133f13dbdd521e794dd876aee7dd
Habeeb Ahmadu <habeebahmadu1@gmail.com>
2022-09-11 16:45:00 +0100

    Updated user schema and deployed sanity studio

--- sanity/schemas/user.js
+++ sanity/schemas/user.js
@@ -1,33 +1,38 @@
 export default {
-	name: 'user',
-	title: 'User',
-	type: 'document',
-	fields: [
-		{
-			name: 'userName',
-			title: 'User Name',
-			type: 'string',
-		},
-		{
-			name: 'fullName',
-			title: 'Full Name',
-			type: 'string',
-		},
-		{
-			name: 'email',
-			title: 'Email',
-			type: 'string',
-		},
-		{
-			name: 'picture',
-			title: 'Picture',
-			type: 'string',
-		},
-		{
-			name: 'orders',
-			title: 'Orders',
-			type: 'array',
-			of: [{ type: 'order' }],
-		},
-	],
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      title: "User Name",
+      type: "string",
+    },
+    {
+      name: "fullName",
+      title: "Full Name",
+      type: "string",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "picture",
+      title: "Picture",
+      type: "string",
+    },
+    {
+      name: "orders",
+      title: "Orders",
+      type: "array",
+      of: [{ type: "order" }],
+    },
+    {
+      name: "isAdmin",
+      title: "Is Admin",
+      type: "boolean",
+    },
+  ],
 };
